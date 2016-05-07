125ee3426385ce0ff447792bfaa0f2485331aa48
HAMED <HAMED@Mohamed-Hamed.mshome.net>
2016-05-07 13:13:15 +0200

    TTT

---
+++ test1-master/src/section_test/test.java
@@ -0,0 +1,16 @@
+package section_test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method();
+	}
+
+	private static void method() {
+		// TODO Auto-generated method stub
+		System.out.println("Hamed!"); // tmam?
+
+	}
+
+}
